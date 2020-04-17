8fc4fd940ce4dc4712351c0f68d3ed3d878157fc
Nareed <45772161+Nareed@users.noreply.github.com>
2020-04-18 02:50:33 +0300

    Added multiclassification thingy

--- SectionC.py
+++ SectionC.py
@@ -4,10 +4,11 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-import dtc
+from dtc import encode_feaures
 
 
-def classification_tree():
+def classification_tree(X_train, X_test, y_train, y_test):
+    print("------------------ C - 1 - Classification Tree ------------------")
     clf = DecisionTreeClassifier()
 
     # Train Decision Tree Classifer
@@ -27,7 +28,8 @@ def classification_tree():
                   'min_samples_split': [2, 3, 4, 5]}
 
     # Create a grid search object
-    gsDCT = GridSearchCV(DecisionTreeClassifier(), param_grid, cv=5, scoring='accuracy')
+    gsDCT = GridSearchCV(DecisionTreeClassifier(), param_grid,
+                         cv=5, scoring='accuracy')
 
     # Fit the grid search
     gsDCT.fit(X_train, y_train)
@@ -41,25 +43,25 @@ def classification_tree():
     print("Accuracy after optimization:", metrics.accuracy_score(y_test, y_pred))
 
 
-def classification_forest():
+def classification_forest(X_train, X_test, y_train, y_test):
+    print("------------------ C - 1 - Classification Forest ------------------")
     # Create a model with 100 trees
-    model = RandomForestClassifier(n_estimators=100,
-                                   max_features='sqrt',
-                                   n_jobs=-1, verbose=1)
+    rfc = RandomForestClassifier(n_estimators=100, max_features='sqrt',
+                                 n_jobs=-1, verbose=1)
 
     # Fit on training data
-    model.fit(X_train, y_train)
+    rfc.fit(X_train, y_train)
     n_nodes = []
     max_depths = []
 
-    for ind_tree in model.estimators_:
+    for ind_tree in rfc.estimators_:
         n_nodes.append(ind_tree.tree_.node_count)
         max_depths.append(ind_tree.tree_.max_depth)
 
     print(f'Average number of nodes {int(np.mean(n_nodes))}')
     print(f'Average maximum depth {int(np.mean(max_depths))}')
 
-    random_pred = model.predict(X_test)
+    random_pred = rfc.predict(X_test)
     print("Accuracy:", metrics.accuracy_score(y_test, random_pred))
 
     # Optimization for the random forest using random search
@@ -72,24 +74,21 @@ def classification_forest():
         'bootstrap': [True, False]  # Whether bootstrap samples are used when building trees.
     }
 
-    # Estimator for use in random search
-    estimator = RandomForestClassifier()
-
-    # Create the random search model
-    rs = RandomizedSearchCV(estimator, param_grid, n_jobs=-1,
-                            scoring='accuracy', cv=5,
-                            n_iter=1, verbose=1)
+    # Create a grid search object
+    gsRFC = GridSearchCV(RandomForestClassifier(), param_grid, n_jobs=-1,
+                         scoring='accuracy', cv=5, verbose=1)
 
     # Fit
-    rs.fit(X_train, y_train)
-    print(rs.best_params_)
+    gsRFC.fit(X_train, y_train)
+    print(gsRFC.best_params_)
 
-    best_model = rs.best_estimator_
+    best_model = gsRFC.best_estimator_
     random_pred = best_model.predict(X_test)
     print("Accuracy after optimization:", metrics.accuracy_score(y_test, random_pred))
 
 
-def regression_tree():
+def regression_tree(X_train, X_test, y_train, y_test):
+    print("------------------ C - 2 - Regression Tree ------------------")
     rdt = DecisionTreeRegressor(random_state=0)
     print("train data is ", X_train)
     print("test data is", y_train)
@@ -124,12 +123,12 @@ def regression_tree():
     print("MSE after optimization:", metrics.mean_squared_error(y_test, y_pred))
 
 
-def regression_forest():
+def regression_forest(X_train, X_test, y_train, y_test):
+    print("------------------ C - 2 - Regression Forest ------------------")
     # Create a model with 100 trees
     regression_model = RandomForestRegressor(n_estimators=100,
                                              max_features='sqrt',
                                              n_jobs=-1, verbose=1)
-
     # Fit on training data
     regression_model.fit(X_train, y_train)
     n_nodes = []
@@ -155,21 +154,98 @@ def regression_forest():
         'bootstrap': [True, False]  # Whether bootstrap samples are used when building trees.
     }
 
-    # Estimator for use in random search
-    estimator = RandomForestRegressor()
-
-    # Create the random search model
-    rs = RandomizedSearchCV(estimator, param_grid, n_jobs=-1,
-                            cv=5,
-                            n_iter=1, verbose=1)
+    # Create a grid search object
+    gsRFC = GridSearchCV(RandomForestRegressor(), param_grid, n_jobs=-1,
+                         cv=5, verbose=1)
 
     # Fit
-    rs.fit(X_train, np.ravel(y_train, order='C'))
-    print(rs.best_params_)
+    gsRFC.fit(X_train, np.ravel(y_train, order='C'))
+    print(gsRFC.best_params_)
 
-    best_model = rs.best_estimator_
+    best_model = gsRFC.best_estimator_
     random_pred = best_model.predict(X_test)
     print("MSE after optimization:", metrics.mean_squared_error(y_test, random_pred))
+
+
+def multiclassification_tree(X_train, X_test, y_train, y_test):
+
+    print("------------------ C - 3 - Multiclassification Tree ------------------")
+
+    dtc = DecisionTreeClassifier()
+
+    # Train Decision Tree Classifer
+    dtc = dtc.fit(X_train, y_train)
+
+    # Predict the response for test dataset
+    y_pred = dtc.predict(X_test)
+    print(f'First decision tree has {dtc.tree_.node_count} nodes with maximum depth {dtc.tree_.max_depth}.')
+
+    # Model Accuracy, how often is the classifier correct?
+    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+    # Create a dictionary of all the parameter options
+    param_grid = {'criterion': ['gini'],
+                  'max_leaf_nodes': list(range(2, 100)),
+                  'max_depth': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+                  'min_samples_split': [2, 3, 4, 5]}
+
+    # Create a grid search object
+    gsDCT = GridSearchCV(DecisionTreeClassifier(), param_grid, cv=5, scoring='accuracy')
+
+    # Fit the grid search
+    gsDCT.fit(X_train, y_train)
+
+    print(gsDCT.best_estimator_)
+
+    # Predict the response for test dataset
+    y_pred = gsDCT.predict(X_test)
+
+    # Model Accuracy, how often is the classifier correct?
+    print("Accuracy after optimization:", metrics.accuracy_score(y_test, y_pred))
+
+
+def multiclassification_forest(X_train, X_test, y_train, y_test):
+    print("------------------ C - 3 - Classification Forest ------------------")
+    # Create a model with 100 trees
+    mcf = RandomForestClassifier(n_estimators=100, max_features='sqrt',
+                                 n_jobs=-1, verbose=1)
+
+    # Fit on training data
+    mcf.fit(X_train, y_train)
+    n_nodes = []
+    max_depths = []
+
+    for ind_tree in mcf.estimators_:
+        n_nodes.append(ind_tree.tree_.node_count)
+        max_depths.append(ind_tree.tree_.max_depth)
+
+    print(f'Average number of nodes {int(np.mean(n_nodes))}')
+    print(f'Average maximum depth {int(np.mean(max_depths))}')
+
+    random_pred = mcf.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, random_pred))
+
+    # Optimization for the random forest using random search
+    # Hyperparameter grid
+    param_grid = {
+        'n_estimators': [100, 150, 200, 250],  # The number of trees in the forest.
+        'max_depth': [None, 50, 60, 70],  # The maximum depth of the tree.
+        'max_features': ['sqrt', None],  # he number of features to consider when looking for the best split
+        'min_samples_split': [2, 5, 10],  # The minimum number of samples required to split an internal node
+        'bootstrap': [True, False]  # Whether bootstrap samples are used when building trees.
+    }
+
+    # Create a grid search object
+    gsRFC = GridSearchCV(RandomForestClassifier(), param_grid, n_jobs=-1,
+                         scoring='accuracy', cv=5, verbose=1)
+
+    # Fit
+    gsRFC.fit(X_train, y_train)
+    print(gsRFC.best_params_)
+
+    best_model = gsRFC.best_estimator_
+    random_pred = best_model.predict(X_test)
+    print("Accuracy after optimization:", metrics.accuracy_score(y_test, random_pred))
 
 
 if __name__ == "__main__":
@@ -180,8 +256,7 @@ if __name__ == "__main__":
     # 3)	Test: rows 24,130-end.
 
     feature_cols = data.columns
-    print(feature_cols)
-    # get all coloumns but the last ( last columns is the class )
+    # get all columns but the last ( last columns is the class )
     feature_cols = feature_cols[:-1]
     X = data[feature_cols]
 
@@ -190,7 +265,7 @@ if __name__ == "__main__":
     # DATA FOR CLASSIFICATION
 
     # encode all categorical features
-    X = dtc.encode_feaures(X)
+    X = encode_feaures(X)
     print(data.columns[-1])
     # class is the last columns
     y = data[data.columns[-1]]
@@ -200,34 +275,52 @@ if __name__ == "__main__":
     y = y.to_numpy()
     X_2 = X.to_numpy()
 
-    X_train, X_test, y_train, y_test = train_test_split(X_2, y, test_size=0.19995, random_state=1)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2111, random_state=1)
+    X1_train, X1_test, y1_train, y1_test = train_test_split(X_2, y, test_size=0.19995, random_state=1)
+    # X1_train, X1_val, y1_train, y1_val = train_test_split(X1_train, y1_train, test_size=0.2111, random_state=1)
 
     # BUILD THE TREE / FOREST
 
-    # classification_tree()
-    # classification_forest()
+    # classification_tree(X1_train, X1_test, y1_train, y1_test)
+    # classification_forest(X1_train, X1_test, y1_train, y1_test)
 
     # (2) Regression using both a tree and forest
 
     # DATA FOR REGRESSION
     cols_to_drop = ['education', 'education-num', 'occupation', '>50K']
-    new_y = X[['education-num']]
-    new_X = data.drop(cols_to_drop, axis=1)
+    y2 = X[['education-num']]
+    X2 = data.drop(cols_to_drop, axis=1)
 
     # encode all categorical features
-    new_X = dtc.encode_feaures(new_X)
+    X2 = encode_feaures(X2)
     # class is the last columns
-    new_X.to_csv("newnewdata.csv")
+    X2.to_csv("newnewdata.csv")
 
-    new_y = new_y.to_numpy()
-    new_X2 = new_X.to_numpy()
+    y2 = y2.to_numpy()
+    X2 = X2.to_numpy()
 
-    X_train, X_test, y_train, y_test = train_test_split(new_X2, new_y, test_size=0.19995, random_state=1)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2111, random_state=1)
+    X2_train, X2_test, y2_train, y2_test = train_test_split(X2, y2, test_size=0.19995, random_state=1)
+    # X2_train, X2_val, y2_train, y2_val = train_test_split(X2_train, y2_train, test_size=0.2111, random_state=1)
 
     # BUILD THE TREE / FOREST
 
-    # regression_tree()
-    # regression_forest()
+    # regression_tree(X2_train, X2_test, y2_train, y2_test)
+    # regression_forest(X2_train, X2_test, y2_train, y2_test)
 
+    # DATA FOR  MULTI-CLASS CLASSIFICATION
+
+    cols_to_drop = ['education', 'education-num', 'occupation', '>50K']
+    y3 = X[['education']]
+    X3 = data.drop(cols_to_drop, axis=1)
+
+    # encode all categorical features
+    X3 = encode_feaures(X3)
+    # class is the last columns
+    X3.to_csv("newnewdata.csv")
+
+    y3 = y3.to_numpy()
+    X3 = X3.to_numpy()
+
+    X3_train, X3_test, y3_train, y3_test = train_test_split(X3, y3, test_size=0.19995, random_state=1)
+
+    # multiclassification_tree(X3_train, X3_test, y3_train, y3_test)
+    # multiclassification_forest(X3_train, X3_test, y3_train, y3_test)
