ae2c23651791812d8d390fc66440fe1181ed4acf
Nareed <nareedhashem@hotmail.com>
2020-04-21 08:23:22 +0300

    fixed run time

--- SectionC.py
+++ SectionC.py
@@ -4,7 +4,7 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-from dtc import encode_feaures
+from Ex1 import encode_feaures
 
 
 def classification_tree(X_train, X_test, y_train, y_test):
@@ -24,7 +24,7 @@ def classification_tree(X_train, X_test, y_train, y_test):
     # Create a dictionary of all the parameter options
     param_grid = {'criterion': ['gini'],
                   'max_leaf_nodes': list(range(2, 100)),
-                  'max_depth': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+                  'max_depth': [13, 14, 15, 16, 17],
                   'min_samples_split': [2, 3, 4, 5]}
 
     # Create a grid search object
@@ -68,10 +68,10 @@ def classification_forest(X_train, X_test, y_train, y_test):
     # Hyperparameter grid
     param_grid = {
         'n_estimators': [100, 150, 200, 250],  # The number of trees in the forest.
-        'max_depth': [None, 50, 60, 70],  # The maximum depth of the tree.
-        'max_features': ['sqrt', None],  # he number of features to consider when looking for the best split
-        'min_samples_split': [2, 5, 10],  # The minimum number of samples required to split an internal node
-        'bootstrap': [True, False]  # Whether bootstrap samples are used when building trees.
+        'max_depth': [None, 10, 20, 30],  # The maximum depth of the tree.
+        'max_features': ['sqrt'],  # he number of features to consider when looking for the best split
+        'min_samples_split': [2, 5, 10, 15],  # The minimum number of samples required to split an internal node
+        'bootstrap': [True]  # Whether bootstrap samples are used when building trees.
     }
 
     # Create a grid search object
@@ -106,7 +106,7 @@ def regression_tree(X_train, X_test, y_train, y_test):
     # Create a dictionary of all the parameter options
     param_grid = {
         'max_leaf_nodes': list(range(2, 100)),
-        'max_depth': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        'max_depth': [13, 14, 15, 16, 17],
         'min_samples_split': [2, 3, 4, 5]}
 
     # Create a grid search object
@@ -148,10 +148,10 @@ def regression_forest(X_train, X_test, y_train, y_test):
     # Hyperparameter grid
     param_grid = {
         'n_estimators': [100, 150, 200, 250],  # The number of trees in the forest.
-        'max_depth': [None, 50, 60, 70],  # The maximum depth of the tree.
-        'max_features': ['sqrt', None],  # he number of features to consider when looking for the best split
-        'min_samples_split': [2, 5, 10],  # The minimum number of samples required to split an internal node
-        'bootstrap': [True, False]  # Whether bootstrap samples are used when building trees.
+        'max_depth': [None, 10, 20, 30],  # The maximum depth of the tree.
+        'max_features': ['sqrt'],  # he number of features to consider when looking for the best split
+        'min_samples_split': [2, 5, 10, 15],  # The minimum number of samples required to split an internal node
+        'bootstrap': [True]  # Whether bootstrap samples are used when building trees.
     }
 
     # Create a grid search object
@@ -168,7 +168,6 @@ def regression_forest(X_train, X_test, y_train, y_test):
 
 
 def multiclassification_tree(X_train, X_test, y_train, y_test):
-
     print("------------------ C - 3 - Multiclassification Tree ------------------")
 
     dtc = DecisionTreeClassifier()
@@ -266,11 +265,10 @@ if __name__ == "__main__":
 
     # encode all categorical features
     X = encode_feaures(X)
-    print(data.columns[-1])
+
     # class is the last columns
     y = data[data.columns[-1]]
     target_col = data.columns[-1]
-    X.to_csv("newdata.csv")
 
     y = y.to_numpy()
     X_2 = X.to_numpy()
@@ -280,8 +278,8 @@ if __name__ == "__main__":
 
     # BUILD THE TREE / FOREST
 
-    # classification_tree(X1_train, X1_test, y1_train, y1_test)
-    # classification_forest(X1_train, X1_test, y1_train, y1_test)
+    classification_tree(X1_train, X1_test, y1_train, y1_test)
+    classification_forest(X1_train, X1_test, y1_train, y1_test)
 
     # (2) Regression using both a tree and forest
 
@@ -292,8 +290,6 @@ if __name__ == "__main__":
 
     # encode all categorical features
     X2 = encode_feaures(X2)
-    # class is the last columns
-    X2.to_csv("newnewdata.csv")
 
     y2 = y2.to_numpy()
     X2 = X2.to_numpy()
@@ -303,8 +299,8 @@ if __name__ == "__main__":
 
     # BUILD THE TREE / FOREST
 
-    # regression_tree(X2_train, X2_test, y2_train, y2_test)
-    # regression_forest(X2_train, X2_test, y2_train, y2_test)
+    regression_tree(X2_train, X2_test, y2_train, y2_test)
+    regression_forest(X2_train, X2_test, y2_train, y2_test)
 
     # DATA FOR  MULTI-CLASS CLASSIFICATION
 
@@ -314,8 +310,6 @@ if __name__ == "__main__":
 
     # encode all categorical features
     X3 = encode_feaures(X3)
-    # class is the last columns
-    X3.to_csv("newnewdata.csv")
 
     y3 = y3.to_numpy()
     X3 = X3.to_numpy()
